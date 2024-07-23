69670a9e2ce2e214bc06420751a85ac6b9b8dc81
Sam Johnston <samj@samj.net>
2024-07-23 15:12:44 -0700

    rm -

--- .github/scripts/verify-gpg-signatures.sh
+++ .github/scripts/verify-gpg-signatures.sh
@@ -19,7 +19,7 @@ done
 
 # Print trusted keys
 echo "Trusted keys:"
-gpg --list-keys ---fingerprint
+gpg --list-keys --fingerprint
 
 # Function to check if a key is trusted or signed by a trusted key
 is_key_trusted_or_signed_by_trusted() {
