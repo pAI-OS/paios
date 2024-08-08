55392048d5a02b040930563147bd6570df7ea1b7
Sanu <sanudutta45@gmail.com>
2024-08-08 23:48:56 +0530

    auth validation function

--- backend/api/AuthView.py
+++ backend/api/AuthView.py
@@ -3,7 +3,7 @@ from backend.managers.AuthManager import AuthManager
 import logging
 from backend.schemas import RegistrationOptions, VerifyAuthentication, AuthenticationOptions, VerifyRegistration
 from connexion import request
-
+from connexion.exceptions import OAuthProblem
 logger = logging.getLogger(__name__)
 
 class AuthView:
@@ -42,6 +42,7 @@ class AuthView:
         response = JSONResponse({"options": options}, status_code=200)
         response.set_cookie(key="challenge", value=challenge)
         return response
+    
     async def verify_authentication(self, body: VerifyAuthentication):
         user = await self.am.signinResponse(body["challenge"], body["email"],body["auth_resp"])
 
@@ -52,4 +53,12 @@ class AuthView:
         response.set_cookie(key="challenge", value="", expires=0)
         response.set_cookie(key="user", value=user)
         return response
+    
+    async def apikey_auth(self, user_id, scopes):
+        user = await self.am.apikey_auth(user_id)
+
+        if not user:
+            raise OAuthProblem("Invalid token")
+        
+        return user
     
