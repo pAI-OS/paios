7d232620c2f1c201fef18be04fa18220ab812892
Sam Johnston <samj@samj.net>
2024-08-09 20:56:09 +0200

    move init_db from each manager to __init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,6 +1,7 @@
 # backend package
 import os
 from common import paths
+from backend.db import init_db
 
 # List of directories to ensure exist
 required_directories = [
@@ -11,3 +12,6 @@ required_directories = [
 for directory in required_directories:
     if not os.path.exists(directory):
         os.makedirs(directory)
+
+# Initialize the database
+init_db()
