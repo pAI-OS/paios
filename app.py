4588ee92d2683aff3ed5aaa324b3d8a3498bd73e
Patricia Bedard <pjbedard@gmail.com>
2024-09-03 18:05:07 -0400

    Share link redirection

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 from backend.app import create_backend_app
 from starlette.staticfiles import StaticFiles
+from backend.redirector import redirector
 
 def create_app():
     app = create_backend_app()
+    add_redirector_app(app)
     add_frontend_app(app)
     return app
 
@@ -14,4 +16,12 @@ def add_frontend_app(app):
         '/{path:path}', 
         endpoint='frontend', 
         view_func=StaticFiles(directory=static_dir, html=True)
+    )
+
+def add_redirector_app(app):
+    # Add a route for handling URL redirection for bot access
+    app.add_url_rule(
+        '/r/{url_key:str}', 
+        endpoint='redirector',
+        view_func=redirector
     )
