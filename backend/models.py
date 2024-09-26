18b43b9a115cf20e80977e49ca2b7f219b207618
Sam Johnston <samj@samj.net>
2024-09-26 10:21:49 -0700

    LargeBinary

--- backend/models.py
+++ backend/models.py
@@ -1,15 +1,19 @@
 from datetime import datetime, timezone
 from uuid import uuid4
 from sqlmodel import Field
-from sqlalchemy import Column, JSON
+from sqlalchemy import Column, LargeBinary
 from backend.db import SQLModelBase
 from typing import Any, ClassVar
 from . import schemas
 
+# Models inherit from SQLModelBase in db.py and the schemas in schemas.py
+# The fields in the schemas are used to generate the columns in the database
+# Any database specific configuration should be done here as an overlay of sorts
+
 class Config(SQLModelBase, schemas.ConfigSchema, table=True):
     id: str = Field(default_factory=lambda: str(uuid4()), primary_key=True)
     key: str = Field(index=True)
-    value: Any = Field(sa_column=Column(JSON))
+    value: bytes = Field(sa_column=Column(LargeBinary))
     version: int = Field(default=1)
     user_id: str | None = Field(default=None, foreign_key="user.id")
     created_at: datetime = Field(default_factory=lambda: datetime.now(tz=timezone.utc))
